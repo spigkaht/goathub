c435315e798e439dd98b43d0c5bf0a3865ad91de
prescot palmer <prescotpalmer4@gmail.com>
2024-06-03 12:41:28 +1000

    CRUD actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+
+  def index
+    @goats = Goat.all
+  end
+
+  def show
+    @goat = Goat.find(params[:id])
+  end
+
+  def new
+    @goat = Goat.new
+  end
+
+  def create
+    @goat = Goat.new(goat_params)
+    @goat = Goat.save
+    # redirect_to...
+  end
+
+  def update
+    @goat.update
+  end
 end
