ac511a03873a0ad4bf3fe36d93369fbf77119f1d
Benjamin Jackson <benj@betterrentals.com.au>
2024-06-04 15:52:10 +1000

    le wagon

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,7 +1,13 @@
 class OffersController < ApplicationController
   # GET /goats/:id/offers/new offers#new
+  def new
+    @goat = Goat.find(params[:goat_id])
+    @offer = Offer.new
+  end
 
   # POST /goats/:id/offers	offers#create
+  def create
+  end
 
   # PATCH /offers/:id/edit	offers#update
 end
