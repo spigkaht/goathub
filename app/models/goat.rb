ae0fed573483c8fa8db8e4782c41181bc38872e6
Benjamin Jackson <benj@betterrentals.com.au>
2024-06-04 11:46:53 +1000

    added cloudinary support

--- app/models/goat.rb
+++ app/models/goat.rb
@@ -1,6 +1,7 @@
 class Goat < ApplicationRecord
   belongs_to :user
   has_many :offers
+  has_one_attached :photo
 
   validates :name, :price, :color, :weight, :personality, :age, presence: true
 end
