9cc8f4b52809c2429ce09660779d7bc191c0f40a
prescot palmer <prescotpalmer4@gmail.com>
2024-06-06 17:07:55 +1000

    failed sound effect

--- app/javascript/controllers/playsound_controller.js
+++ app/javascript/controllers/playsound_controller.js
@@ -1,7 +1,31 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="playsound"
+import correctSound from "./assets/sounds/Bahhhh.mp3";
+
+// Connects to data-controller="sfx"
 export default class extends Controller {
+  static targets = ["sound"]
+  static values = {
+    correct: Boolean
+  }
+
   connect() {
+    console.log("connected")
+    console.log(this.correctValue)
+    if (this.correctValue) {
+      this.playCorrectSound();
+    }
+  }
+
+
+  playCorrectSound() {
+    try {
+      // I pull the src value from the audio tag
+      const url = this.soundTarget.src;
+      const audio = new Audio(url);
+      audio.play();
+    } catch (error) {
+      console.error("Failed to play sound:", error);
+    }
   }
 }
